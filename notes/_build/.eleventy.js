fa4594df39b08fa03aa3083228abee059c645e41
alifeee <alifeeerenn@hotmail.com>
2024-11-02 17:17:58 +0000

    move files down one directory

--- notes/_build/.eleventy.js
+++ notes/_build/.eleventy.js
@@ -2,11 +2,16 @@ import handlebarsPlugin from "@11ty/eleventy-plugin-handlebars";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("..");
+  eleventyConfig.ignores.add("../_build/node_modules");
+  eleventyConfig.ignores.add("../_build/_site");
+  eleventyConfig.watchIgnores.add("../_build/node_modules");
+  eleventyConfig.watchIgnores.add("../_build/_site");
+
   if (process.env.ELEVENTY_RUN_MODE != "build") {
-    eleventyConfig.addPassthroughCopy({ "../picnic.css": "/" });
+    eleventyConfig.addPassthroughCopy({ "../../picnic.css": "/" });
   } else if (process.env.ELEVENTY_RUN_MODE == "build") {
-    eleventyConfig.setInputDirectory(".");
-    eleventyConfig.setOutputDirectory(".");
+    eleventyConfig.setOutputDirectory("..");
   }
 
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
