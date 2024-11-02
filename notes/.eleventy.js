6b98a24102f151eaba2eb0d8a531620829d43028
alifeee <alifeeerenn@hotmail.com>
2024-11-02 16:46:39 +0000

    change base URL (to deploy to subdirectory)

--- notes/.eleventy.js
+++ notes/.eleventy.js
@@ -1,4 +1,5 @@
 import handlebarsPlugin from "@11ty/eleventy-plugin-handlebars";
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   if (process.env.ELEVENTY_RUN_MODE != "build") {
@@ -8,5 +9,10 @@ export default function (eleventyConfig) {
     eleventyConfig.setOutputDirectory(".");
   }
 
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(handlebarsPlugin);
 }
+
+export const config = {
+  pathPrefix: process.env.ELEVENTY_RUN_MODE == "build" ? "/notes/" : "/",
+};
