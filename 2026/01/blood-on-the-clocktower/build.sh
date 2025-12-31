4b6e5fd51d28240ba1b061ac1980d6fac38b132f
alifeee <alifeee@alifeee.net>
2025-12-31 19:50:14 +0000

    finish info for zoomy image and template some other bits

--- 2026/01/blood-on-the-clocktower/build.sh
+++ 2026/01/blood-on-the-clocktower/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # build HTML !
 
+# check marked is installed
+if ! type "marked" 2> /dev/null > /dev/null; then
+  echo "marked not installed. https://www.npmjs.com/package/marked. please run"
+  echo "npm install -g marked"
+  exit 1
+fi
+
 INDEX_FILE="index.html"
 TEMP_FILE="index.html.temp"
+CONTENT_FILE="index.md"
 START_CONTENT="<!-- !!start content!! -->"
 END_CONTENT="<!-- !!end content!! -->"
 original_html=$(cat $INDEX_FILE)
@@ -10,13 +18,27 @@ original_html=$(cat $INDEX_FILE)
 json=$(
     cat partsinfo.json
 )
+
+cat << EOHTML > info.html
+<!DOCTYPE html>
+<html><head><style>
+body {max-width:40rem;}
+</style></head>
+<body><a href=".">back</a>
+EOHTML
+echo "${json}" | jq -r \
+  '.[] | .links[] |= "<li><a href=\"parts/\(.)\">\(.)</a></li>" | .links |= if length > 0 then join("") else "" end | to_entries | ("<hr>", (.[] | if .value != "" then ("<dl><dt>\(.key)</dt><dd>\(.value)</dd></dl>") else "" end))' \
+  >> info.html
+echo "</body></html>" >> info.html
+
+
 html=$(
     # svg
     echo "<div id=contents>"
     cat images/box_contents.svg \
       | sed 's+xlink:href="box_contents.webp"+xlink:href="images/box_contents.webp"+' \
       | sed 's+fill:#ff0000;fill-opacity:0.5+fill:var(--f);fill-opacity:var(--o)+'
-    echo "<label><input type=checkbox id=showall autocomplete=off>highlight</label>"
+    echo "<label><input type=checkbox id=showall autocomplete=off>highlight all</label>"
     while read partfile; do
         fn="${partfile##*/}"
         bn="${fn%.*}"
@@ -26,12 +48,15 @@ html=$(
         echo "  <img src='${partfile}' alt='picture of board game pieces: ${bn}'>"
         echo "  <form method=dialog><button autofocus role=dialog>×</button></form>"
         echo "  <div class=info>"
-        echo "${json}" | jq -r '.'"${bn}"' | .links[] |= "<a href=\"parts/\(.)\">\(.)</a>" | .links |= join("") | "    <span class=name>\(.name)</span>\n    <span class=madewith>\(.madewith)</span>\n    <span class=links>\(.links)</span>\n    <p class=description>\(.description)</p>"'
+        echo "${json}" | jq -r '.'"${bn}"' | .links[] |= "<li><a href=\"parts/\(.)\">\(.)</a></li>" | .links |= if length > 0 then join("") else "" end | "    <span class=name>\(.name)</span>\n    <span class=madewith>\(.madewith)</span>\n    <ul class=links>\(.links)</ul>\n    <p class=description>\(.description)</p>"'
         echo "  </div>"
         echo "</dialog>"
     done <<< $(find images/parts -type f)
-    echo "<div id=debugdot></div>"
     echo "</div>"
+    echo "<p>see <a href='info.html'>all this information on one page</a> or see the underlying <a href='partsinfo.json'>JSON file</a></p>"
+    echo "<section id=markdown>"
+    marked -i "$CONTENT_FILE"
+    echo "</section>"
 )
 
 # get line numbers of START_CONTENT and END_CONTENT
---
+++ 2026/01/blood-on-the-clocktower/index.md
@@ -0,0 +1,33 @@
+
+-----
+
+***content below unfinished***
+
+-----
+
+### laser cutting
+
+![](images/lightburn.png)
+
+### box shots
+
+![](./images/box_top.webp)
+![](./images/box_side.webp)
+![](./images/box_partial.webp)
+![](./images/box_open.webp)
+
+### demon face
+
+![](./images/face.webp)
+
+### official boxes
+
+![](images/official_box-inside.webp)
+
+![](images/official_box-contents1.webp)
+![](images/official_box-contents3.webp)
+![](images/official_box-contents-2.webp)
+
+### to-do
+
+- add images here from camera roll :]
